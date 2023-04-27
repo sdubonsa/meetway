893c94b8a7de3e48a4de0bfb01bfb34718f0f893
Santiago <santiagodubon@outlook.com>
2023-04-27 18:08:17 -0400

    Login and signup validation

--- middleware/validator.js
+++ middleware/validator.js
@@ -7,3 +7,51 @@ exports.validateId = (req, res, next) => {
     }
     return next();
 }
+
+exports.validateSignUp = (req, res, next) => {
+    [
+        body("firstName", "Please enter your first name")
+            .trim()
+            .escape()
+            .notEmpty(),
+        body("lastName", "Please enter your last name")
+            .trim()
+            .escape()
+            .notEmpty(),
+        body("email", "Please enter a valid email address")
+            .isEmail()
+            .trim()
+            .escape()
+            .normalizeEmail(),
+        body(
+            "password",
+            "Please enter a password with 8 or more characters, max of 64 characters"
+            ).isLength({ min: 8, max: 64 }),
+    ];
+};
+
+exports.validateLogin = (req, res, next) => {
+    [
+        body("email", "Please enter a valid email address")
+            .isEmail()
+            .trim()
+            .escape()
+            .normalizeEmail(),
+        body(
+            "password",
+            "Please enter a password with 8 or more characters, max of 64 characters"
+        ).isLength({ min: 8, max: 64 }),
+    ];
+};
+
+exports.validateResult = (req, res, next) => {
+  let errors = validationResults(req);
+  if (!errors.isEmpty()) {
+    errors.array().forEach((error) => {
+      req.flash("error", error);
+    });
+    return res.redirect("back");
+  } else {
+    return next();
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const controller = require('../controllers/userController');
 const { isGuest, isLoggedIn } = require('../middleware/auth');
 const { logInLimiter } = require("../middleware/rateLimiter");
+const { validateLogin, validateSignUp, validateResult } = require("../middleware/validator");
 
 const router = express.Router();
 
@@ -9,13 +10,13 @@ const router = express.Router();
 router.get('/new', isGuest, controller.new);
 
 //POST /users: create a new user account
-router.post('/', isGuest, controller.create);
+router.post('/', isGuest, validateSignUp, validateResult, controller.create);
 
 //GET /users/login: send html for logging in
 router.get('/login', isGuest, controller.getUserLogin);
 
 //POST /users/login: authenticate user's login
-router.post('/login', isGuest, logInLimiter, controller.login);
+router.post('/login', isGuest, logInLimiter, validateLogin, validateResult, controller.login);
 
 //GET /users/profile: send user's profile page
 router.get('/profile', isLoggedIn, controller.profile);
